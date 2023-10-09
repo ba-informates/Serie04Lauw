e2532b41c73c29a03791b3a8ea483e91a57ec1f2
Lauw <Laura.paraboschi@hotmail.com>
2023-10-10 01:01:31 +0200

    Debut de la série

---
+++ src/Codes.java
@@ -0,0 +1,11 @@
+public class Unicode {
+    public static void main(String[] args) {
+        //System.out.println("Unicode de * : " + String.toH);
+        //System.out.println("Unicode de * : " + Integer.parseInt("9", 16));
+        //System.out.println("Unicode de * : " + Integer.parseInt("W", 16));
+        System.out.println("Caractère correspondant à l'unicode 34 : " + Character.toChars(34));
+        System.out.println("Caractère correspondant à l'unicode 65 : " + Character.toChars(65));
+        System.out.println("Caractère correspondant à l'unicode 123 : " + Character.toChars(123));
+
+    }
+}
