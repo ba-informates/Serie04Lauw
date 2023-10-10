37a319776a5ca3c454ed011e21837d68bb1eeed8
Lauw <Laura.paraboschi@hotmail.com>
2023-10-10 16:44:36 +0200

    Fin de série!

---
+++ src/MostFrequent.java
@@ -0,0 +1,17 @@
+class MostFrequent {
+
+    public static void main(String[] args) {
+        int[] tab1 = {2, 7, 5, 6, 7, 1, 6, 2, 1, 7, 6};
+        int taille = tab1.length;
+
+        /*******************************************
+         * Completez le programme a partir d'ici.
+         *******************************************/
+
+
+        /*******************************************
+         * Ne rien modifier apres cette ligne.
+         *******************************************/
+
+    }
+}
